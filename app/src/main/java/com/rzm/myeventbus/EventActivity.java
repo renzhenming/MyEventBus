dc1f1e71eef6815d86d04444cc6e27702d741b10
renzhenming <Renzhenming1991>
2018-05-13 11:43:49 +0800

    finish

--- app/src/main/java/com/rzm/myeventbus/EventActivity.java
+++ app/src/main/java/com/rzm/myeventbus/EventActivity.java
@@ -4,16 +4,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class EventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-
+        EventBus.getDefault().register(this);
     }
 
     public void set(View view) {
-        EventTransition.getInstance().post("哈哈");
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                EventTransition.getInstance().post("哈哈");
+                EventBus.getDefault().post("EventBus");
+            }
+        }.start();
+
+    }
+
+    @org.greenrobot.eventbus.Subscribe(threadMode = org.greenrobot.eventbus.ThreadMode.MAIN,priority = 50,sticky = true)
+    public void testEventActivity(String msg){
     }
 }
--- app/src/main/java/com/rzm/myeventbus/MainActivity.java
+++ app/src/main/java/com/rzm/myeventbus/MainActivity.java
@@ -6,16 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private TextView mTextView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.text);
+        mTextView1 = findViewById(R.id.text1);
         EventTransition.getInstance().register(this);
+        EventBus.getDefault().register(this);
     }
 
     public void jump(View view) {
@@ -27,9 +32,20 @@ public class MainActivity extends AppCompatActivity {
         mTextView.setText(msg);
     }
 
+    @org.greenrobot.eventbus.Subscribe(threadMode = org.greenrobot.eventbus.ThreadMode.MAIN,priority = 50,sticky = true)
+    public void test2(String msg){
+        mTextView1.setText(msg);
+    }
+
+    @org.greenrobot.eventbus.Subscribe(threadMode = org.greenrobot.eventbus.ThreadMode.MAIN,priority = 50,sticky = true)
+    public void test3(String msg){
+        mTextView1.setText(msg);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         EventTransition.getInstance().unRegister(this);
+        EventBus.getDefault().unregister(this);
     }
 }
